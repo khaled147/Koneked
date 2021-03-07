04fa3bbe04fc83e88e4118bddc450da232217e6e
Khaled Elmalawany <elmalawanykhaled@gmail.com>
2021-03-07 06:32:38 -0700

    Edited bluetooth connection

--- Mobile_Application/app/src/main/java/com/example/koneked/ui/home/HomeViewModel.java
+++ Mobile_Application/app/src/main/java/com/example/koneked/ui/home/HomeViewModel.java
@@ -1,9 +1,11 @@
 package com.example.koneked.ui.home;
 
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-import com.example.koneked.R;
 
 public class HomeViewModel extends ViewModel {
 
@@ -15,6 +17,19 @@ public class HomeViewModel extends ViewModel {
 
     public void setText(MutableLiveData<String> text) {
         mText = text;
+    }
+
+    public void setImage(ImageView img, int id, String contentDescription) {
+        img.setImageResource(id);
+        img.setContentDescription(contentDescription);
+    }
+
+    public void setButton(Button bt, boolean state) {
+        bt.setEnabled(state);
+    }
+
+    public void setProgressBarVisibility(ProgressBar bar, int state) {
+        bar.setVisibility(state);
     }
 
     public LiveData<String> getText() {
